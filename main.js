70e9ba3a164551c162308bb53a8825844b1729aa
HuyNguyen09196 <huynguyen09196@gmail.com>
2022-10-20 19:25:45 -0500

    change

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var $body=$("body");
 $body.css("backgroundColor","#212529")
-var $main=$("<div id='result' ></div>");
-var $card=$("<div class='card'></div>")
+var $account=$("<div class='container text-center' ></div>");
+var $card=$("<div class='layout'></div>")
 $body.append($card);
-$card.append($main)
+$body.append($account);
 var $result=$("#result");
 var $input= $("#input").val();
 console.log($input);
@@ -21,16 +21,16 @@ $(document).ready(function(){
       var $div=$("<div class='card' style='width: 18rem;'></div>");
       var $img=$("<img class='card-img-top' height='280px'></img>");
       $img.attr("src",datas.images.jpg.image_url);
-      var $h=$("<h4 id='text' class='card-title'></h4>");
+      var $h=$("<h5 id='text' class='text-break'></h5>");
       var $p=$("<p id='text' class='card-text'></p>");
-      var $a=$("<a id='abc' class='btn btn-primary'>Watch Now</a>");
-      var $a2=$("<a id='abcd' class='btn btn-primary'>Trailer</a>");
+      var $a=$("<a id='abc' class='btn btn-light'>Watch Now</a>");
+      var $a2=$("<a id='abcd' class='btn btn-light'>Trailer</a>");
       $a.attr("href",datas.url);
       $h.text(datas.title);
       $p.text(datas.genres[0].name,);
       $a2.attr("href",datas.trailer.url)
       var $div2=$("<div id='bd' class='card-body'></div>");
-      $result.append($div);
+      $card.append($div);
       $div.append($img);
       $div.append($div2);
       $div2.append($h);
@@ -64,16 +64,16 @@ function search(e){
       var $div=$("<div class='card' style='width: 18rem;'></div>");
       var $img=$("<img class='card-img-top' height='350px'></img>");
       $img.attr("src",datas.images.jpg.image_url);
-      var $h=$("<h5 id='text' class='card-title'></h5>");
+      var $h=$("<h5 id='text' class='text-break'></h5>");
       var $p=$("<p id='text' class='card-text'></p>");
-      var $a=$("<a id='abc' class='btn btn-primary'>Watch Now</a>");
-      var $a2=$("<a id='abcd' class='btn btn-primary'>Trailer</a>");
+      var $a=$("<a id='abc' class='btn btn-light'>Watch Now</a>");
+      var $a2=$("<a id='abcd' class='btn btn-light'>Trailer</a>");
       $a.attr("href",datas.url);
       $a2.attr("href",datas.trailer.url)
       $h.text(datas.title);
       $p.text(datas.genres[0].name,);
       var $div2=$("<div id='bd' class='card-body' ></div>");
-      $result.append($div);
+      $card.append($div);
       $div.append($img);
       $div.append($div2);
       $div2.append($h);
@@ -86,53 +86,30 @@ function search(e){
     }
   });
 }
+////////////////////////////////////////////
+
+$("#login").on("click",function(){
+  $("#loginHolder").remove();
+  $("#carouselExampleIndicators").hide()
+  clear();
+  var $loginHolder=$("<div id='loginHolder' class='card-body'></div>")
+  var $div=$("<div id='user' class='form-floating mb-3'></div>")
+  var $input=$("<input type='email' class='form-control' id='floatingInput' placeholder='User Name'>")
+  var $label=$("<label for='floatingInput'>Email address</label>")
+  $div.append($input)
+  $div.append($label)
+  var $div2=$("<div class='form-floating'></div>")
+  var $input2=$("<input type='password' class='form-control' id='floatingPassword' placeholder='Password'>")
+  var $label2=$("<label for='floatingPassword'>Password</label>")
+  var $a=$("<a id='input1' class='btn btn-primary' href='index.html' role='button'>Login</a>")
+  $div2.append($input2)
+  $div2.append($label2)
+  $loginHolder.append($div)
+  $loginHolder.append($div2)
+  $loginHolder.append($a)
+  $account.append($loginHolder)
+})
 
 function clear(){
-  $result.empty();
+  $card.empty();
 }
-
-
-// $("#search").on("click",search)
-// var $input= $("#input").val();
-// console.log($input);
-// function search(e){
-//   e.preventDefault();
-//   $.get("https://api.jikan.moe/v4/anime?q="+ $input ,function(data){
-//     console.log(data)
-//     console.log($input)
-// // $.get("https://api.jikan.moe/v4/anime?q="+ "demon slayer",function(data){
-//   console.log(data.data[0]);
-//   var datas=data.data
-//   for( var j =0;j<datas.length;j++){
-//     // anime(data[i]);   
-// //   }
-// //   function anime(data){
-//     var $span=$("<span class='card'></span>");
-//     var $h1=$("<h1 class='title'></h1>");
-//     var $img=$("<img class='image'></img>");
-//     var $h2=$("<h2 class='genres'></h2>");
-//     var $div=$("<div class='sumary'></div>");
-//     var $p=$("<p></p>");
-//     var $a=$("<a>view anime</a>");
-//     var $p=$("<p>synopsis</p>")
-
-//     $h1.text(datas[j].title);
-//     $img.attr("src",datas[j].images.jpg.image_url)
-//     $h2.text(datas[j].genres[0].name)
-//     $div.append($p)
-//     $p.text(datas[j].synopsis)
-//     $a.attr("href",datas[j].url)
-
-    
-
-
-//     $result.append($span);
-//     $span.append($h1);
-//     $span.append($img)
-//     $span.append($h2)
-//     $span.append($div)
-//     $span.append($a)
-    
-
-//   }
-// });
